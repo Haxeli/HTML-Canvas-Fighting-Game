911f6016ea53473cbb47aa3c3c15a7c4f36aa657
Aleksi <79052109+Haxeli@users.noreply.github.com>
2023-12-28 15:04:01 +0200

    Added game timer and game over functionality

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ class Sprite {
         c.fillStyle = this.color
         c.fillRect(this.position.x, this.position.y, this.width, this.height)
 
-        //attack box
+        // attack box
         if (this.isAttacking) {
             c.fillStyle = 'green'
             c.fillRect(
@@ -124,6 +124,32 @@ function rectangularCollision({rectangle1, rectangle2}) {
     )
 }
 
+function determineWinner({player, enemy, timerId}) {
+    clearTimeout(timerId)
+    document.querySelector('#displayText').style.display = 'flex'
+    if (player.health === enemy.health) {
+        document.querySelector('#displayText').innerHTML = 'Tie!'
+    } else if (player.health > enemy.health) {
+        document.querySelector('#displayText').innerHTML = 'Player 1 Wins!'
+    } else if (player.health < enemy.health) {
+        document.querySelector('#displayText').innerHTML = 'Player 2 Wins!'
+    }
+}
+
+let timer = 60
+let timerId
+function decreaseTimer() {
+    if (timer > 0) {
+        timerId = setTimeout(decreaseTimer, 1000)
+        timer--
+        document.querySelector('#timer').innerHTML = timer
+    }
+
+    if (timer === 0) determineWinner({player, enemy, timerId})
+}
+
+decreaseTimer()
+
 function animate() {
     window.requestAnimationFrame(animate)
     c.fillStyle = 'black'
@@ -134,7 +160,7 @@ function animate() {
     player.velocity.x = 0
     enemy.velocity.x = 0
 
-    //player movement
+    // player movement
     if (keys.a.pressed && player.lastKey === 'a') {
         player.velocity.x = -5
     } else if (keys.d.pressed && player.lastKey === 'd') {
@@ -148,7 +174,7 @@ function animate() {
         enemy.velocity.x = 5
     }
 
-    //detect for collision
+    // detect for collision
     if (
         rectangularCollision({
             rectangle1: player,
@@ -171,6 +197,11 @@ function animate() {
         enemy.isAttacking = false
         player.health -= 10
         document.querySelector('#playerHealth').style.width = player.health + '%'
+    }
+
+    // end game based on health
+    if (enemy.health <= 0 || player.health <= 0) {
+        determineWinner({player, enemy, timerId})
     }
 }
 
@@ -221,7 +252,7 @@ window.addEventListener('keyup', (event) => {
             break
     }
 
-    //enemy controls
+    // enemy controls
     switch (event.key) {
         case 'ArrowRight':
             keys.ArrowRight.pressed = false
