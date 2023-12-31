73728e593c9dba7f5174ad984bab4a54f6de3e3c
Aleksi <79052109+Haxeli@users.noreply.github.com>
2023-12-31 16:32:37 +0200

    Added animations for taking damage

--- js/classes.js
+++ js/classes.js
@@ -143,6 +143,11 @@ class Fighter extends Sprite {
         this.isAttacking = true
     }
 
+    takeHit() {
+        this.switchSprite('takeHit')
+        this.health -= 10
+    }
+
     spriteHandler(sprite) {
         if (this.image !== this.sprites[sprite].image) {
             console.log(`Switched to ${sprite} sprite`)
@@ -153,8 +158,15 @@ class Fighter extends Sprite {
     }
 
     switchSprite(sprite) {
+        // override other animations with attack animation
         if (this.image === this.sprites.attack1.image && this.framesCurrent < this.sprites.attack1.framesMax - 1) return
+        // override other animations with takeHit animation
+        if (this.image === this.sprites.takeHit.image && this.framesCurrent < this.sprites.takeHit.framesMax - 1) return
         switch(sprite) {
+            case (sprite):
+                this.spriteHandler(sprite)
+                break
+            /*
             case 'idle':
                 this.spriteHandler(sprite)
                 break
@@ -170,6 +182,10 @@ class Fighter extends Sprite {
             case 'attack1':
                 this.spriteHandler(sprite)
                 break
+            case 'takeHit':
+                this.spriteHandler(sprite)
+                break
+            */
         }
     }
 }
